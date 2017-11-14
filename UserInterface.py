095d3a1a825181fcf27f7a9c534c2e1c774ec918
Ronald Rounsifer <ronrounsifer@me.com>
2017-11-14 13:33:17 -0500

    Removing errors

--- UserInterface.py
+++ UserInterface.py
@@ -4,42 +4,19 @@ Created on Jan 8, 2017
 @author: rounsifr
 '''
 from Tkinter import *
-from com.gvsubusroutes import BusArrival
-from numpy.core.defchararray import title
+import busTimes as b
+import time
 
+def run():
+	root = Tk()
+	next_stop_time = ''
+	clock = Label(root, font=('arial', 300, 'bold'),fg='white', bg='blue')
+	clock.pack(fill=BOTH, expand=1)
 
-## MAIN WINDOW
-root = Tk()                             
+	def update():
+		next_stop_time = b.run()
+		clock.config(text=next_stop_time)
+		clock.after(200, update)
 
-## VARIABLES
-time = BusArrival.nextarrival('Trio')
-
-## BACKGROUND IMAGE
-background_image = PhotoImage('/home/rounsifr/pi/background5')
-background_label = Label(root, image=background_image)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-## TITLE
-root.title("GVSU Bus Arrival Times")
-
-## LABELS
-triolbl = Label(root, text="Trio")
-triolbl.grid(row=0,column=0)
-label_trio = Label(root, text=BusArrival.nextarrival('Trio'))
-label_trio.grid(row=0,column=1)
-
-triolbl = Label(root, text="Copper")
-triolbl.grid(row=1,column=0)
-label_trio = Label(root, text=BusArrival.nextarrival('Copper'))
-label_trio.grid(row=1,column=1)
-
-triolbl = Label(root, text="Pierce")
-triolbl.grid(row=2,column=0)
-label_trio = Label(root, text=BusArrival.nextarrival('Pierce'))
-label_trio.grid(row=2,column=1)
-
-
-
-
-
-root.mainloop()
+	update()
+	root.mainloop()
